0017119263b0f7e604ce8e2a38e749b0f59404cc
DomokosTibor <33492921+DomokosTibor@users.noreply.github.com>
2018-04-13 17:01:58 +0200

    Task 2 Done

---
+++ task2.sql
@@ -0,0 +1,6 @@
+SELECT companyname AS Company, count(*) AS NumberOfProducts
+FROM suppliers
+INNER JOIN products
+	ON products.supplierid = suppliers.supplierid
+GROUP BY companyname
+ORDER BY count(*) DESC;
